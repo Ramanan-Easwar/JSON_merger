7e730d5e69196b9a622cbee3b013cc604b4c720f
Ramanan <ramananeaswar98@gmail.com>
2019-11-06 21:18:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a python script that merges multiple json files and keeps writing as
+output files in the same folder as the input.
---
+++ final.py
@@ -0,0 +1,60 @@
+import os
+import json
+
+# path = "/home/ramanan/Downloads/test"
+DIR = input("Enter path:")
+DIR += "/"
+output_path = DIR
+file_count = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+name = input("Enter input file basename: ")
+out_name = input("Enter output file base:")
+size = input("Enter the max size in bytes")
+b = bytes(size, 'utf-8')
+DIR += name
+ext = ".json"
+base_num = 1 
+w_num = 1;
+
+
+with open(DIR + "1.json") as json_file:
+	S = json.load(json_file)
+
+res = json.dumps(S)
+i = 2
+key = ""
+while(res[i] != '"'):
+	key += res[i]
+	i += 1
+print("Key: ", key)
+
+json_file.close()
+
+
+
+file_count = int(file_count)
+# print("Number of json files: ", file_count)
+
+result = {}
+result[key] = []
+
+for i in range(file_count):
+	str_num = str(base_num)
+	open_path = DIR + str_num + ext
+	base_num += 1
+	with open(open_path) as json_file:
+		data = json.load(json_file)
+	for s in data[key]:
+		# print("writing :",s['name'])
+		result[key].append(s)
+	if(i == 0):
+		continue
+	write_name = out_name
+	write_name += str(w_num)
+	write_name += ext
+
+	with open(output_path + write_name,'w',) as outfile:
+		json.dump(result,outfile)
+	w_num += 1
+	outfile.close()
+	json_file.close()
+
